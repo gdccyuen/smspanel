fc204b7b7ddd6ffa3e96d7d71ad7e877dc80c4bb
Gordon YUEN <gdccyuen@alumni.cuhk.net>
2026-01-19 12:13:07 +0800

    refactor: update service import to smspanel

--- tests/test_hkt_sms.py
+++ tests/test_hkt_sms.py
@@ -2,7 +2,7 @@
 
 from unittest.mock import patch
 
-from ccdemo.services.hkt_sms import HKTSMSService
+from smspanel.services.hkt_sms import HKTSMSService
 from .mocks import MockHKTPost, MockHKTResponse
 
 
