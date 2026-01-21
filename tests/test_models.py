3a64c41ec33133c7d5b27e7708ddab3df0dc28ca
Gordon YUEN <gdccyuen@alumni.cuhk.net>
2026-01-21 17:35:09 +0800

    models: verify DeadLetterMessage export

---
+++ tests/test_models.py
@@ -0,0 +1,6 @@
+
+
+def test_dead_letter_message_exported():
+    """DeadLetterMessage should be exported from models."""
+    from smspanel.models import DeadLetterMessage
+    assert DeadLetterMessage is not None
